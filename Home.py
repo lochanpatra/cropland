cb58d4af3689ad2b5b8f41ff3821468c9ea2064e
Lochan patra <143163989+lochanpatra@users.noreply.github.com>
2024-12-16 18:24:16 +0530

    Update Home.py

--- Home.py
+++ Home.py
@@ -10,7 +10,7 @@ markdown = """
 
 st.sidebar.title("About")
 st.sidebar.info(markdown)
-logo = "https://emojipedia.org/laptop"
+logo = "https://cdn.pixabay.com/photo/2017/07/27/09/56/sphere-stone-2544690_640.png"
 st.sidebar.image(logo)
 
 # Customize page title
